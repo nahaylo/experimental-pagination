f431388ce023a1b8fb645660745a06412b3c7fab
Vitaliy Nahaylo <nahaylo@gmail.com>
2024-03-14 17:08:44 +0200

    Add partitioned_table gem

--- db/migrate/20231214095550_add_column_created_at_brin.rb
+++ db/migrate/20231214095550_add_column_created_at_brin.rb
@@ -1,4 +1,6 @@
 class AddColumnCreatedAtBrin < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
     add_column :miner_data, :created_at_brin, :datetime, null: false
     add_index :miner_data, :created_at_brin, using: :brin
--- db/migrate/20231214095958_add_user_id.rb
+++ db/migrate/20231214095958_add_user_id.rb
@@ -1,4 +1,6 @@
 class AddUserId < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
     add_column :miner_data, :user_id, :integer, null: false
     add_index :miner_data, :user_id, algorithm: :concurrently
